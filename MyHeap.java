e6d07f79a1acdb31794b7b4272f0ab59603218df
Jacob Olin <jolin00@stuy.edu>
2019-04-29 11:46:38 -0400

    Wrote heapify

--- MyHeap.java
+++ MyHeap.java
@@ -26,4 +26,10 @@ public class MyHeap{
     data[ind1] = data[ind2];
     data[ind2] = k;
   }
+
+  public static void heapify(int[] data) {
+    for (int i = data.length - 1;i >= 0;i -= 1) {
+      pushUp(data,i);
+    }
+  }
 }
