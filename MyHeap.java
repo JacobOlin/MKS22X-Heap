7cfae9225685e5b57288577ce57ded19447b6496
Jacob Olin <jolin00@stuy.edu>
2019-04-29 11:33:00 -0400

    wrote the pushDown function without swap

--- MyHeap.java
+++ MyHeap.java
@@ -1,5 +1,16 @@
 public class MyHeap{
   private static void pushDown(int[]data,int size,int index) {
+    while ((2*index+1 < size && data[2*index+1] > data[index]) || (2*index+2 < size && data[2*index+2] > data[index])) {
+      if (2*index+2 < size && data[2*index+2] > data[2*index+1]) {
+        swap(data,index,2*index+2);
+      }
+      else{
+        swap(data,index,2*index+1);
+      }
+    }
+  }
+
+  private static void swap(int[]data,int ind1,int ind2) {
     
   }
 }
