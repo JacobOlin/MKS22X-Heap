20af1690f3572dca37aeb7b7791b43e24de5d280
Jacob Olin <jolin00@stuy.edu>
2019-04-29 11:34:22 -0400

    Wrote swap and changed index in pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -3,14 +3,18 @@ public class MyHeap{
     while ((2*index+1 < size && data[2*index+1] > data[index]) || (2*index+2 < size && data[2*index+2] > data[index])) {
       if (2*index+2 < size && data[2*index+2] > data[2*index+1]) {
         swap(data,index,2*index+2);
+        index = 2*index+2;
       }
       else{
         swap(data,index,2*index+1);
+        index = 2*index+1;
       }
     }
   }
 
   private static void swap(int[]data,int ind1,int ind2) {
-    
+    int k = data[ind1];
+    data[ind1] = data[ind2];
+    data[ind2] = k;
   }
 }
